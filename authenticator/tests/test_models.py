c4b7a4c269ca251b05305368e51dd473e6c50d0f
Thiago Campos <commit@thigcampos.com>
2024-06-10 14:36:45 -0300

    test: add tests for abstract user

---
+++ authenticator/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(email="testuser@mail.com", password="foobar")
+
+        assert user.email == "testuser@mail.com"
+        assert user.is_active
+        assert not user.is_staff
+        assert not user.is_superuser
+        try:
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email="", password="foobar")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email="testsuperuser@mail.com", password="foobar")
+
+        assert admin_user.email == "testsuperuser@mail.com"
+        assert admin_user.is_active
+        assert admin_user.is_staff
+        assert admin_user.is_superuser
+        try:
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="testsuperuser@mail.com", password="foobar", is_superuser=False)
