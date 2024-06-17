b7f05328b6007bed2e4fef8e595657b45c7d6c2c
Thiago Campos <commit@thigcampos.com>
2024-06-17 14:29:42 -0300

    feat: include missing tests

--- gestaoacademica/tests/test_views.py
+++ gestaoacademica/tests/test_views.py
@@ -15,6 +15,7 @@ from gestaoacademica.models import (
     Professor,
     Sala,
     Participacao,
+    ListaDeEspera,
 )
 
 
@@ -23,10 +24,12 @@ class TestAlunoCreateView(TestCase):
         self._url = reverse("alunos_create")
 
     def test_aluno_create_view_context(self):
+        # Teste para verificar se o formulário de criação de usuário está presente no contexto
         response = self.client.get(self._url)
         assert response.context_data.get("user_creation_form") == UserCreationForm
 
     def test_aluno_create_view_post(self):
+        # Teste para verificar a criação de um novo aluno
         post_data = {
             "nome": "some_string",
             "sobrenome": "other_string",
@@ -38,8 +41,8 @@ class TestAlunoCreateView(TestCase):
         response = self.client.post(self._url, data=post_data)
 
         assert response.status_code == HTTPStatus.FOUND
-        assert User.objects.filter(email=post_data.get("email"))
-        assert Aluno.objects.filter(prontuario=post_data.get("prontuario"))
+        assert User.objects.filter(email=post_data.get("email")).exists()
+        assert Aluno.objects.filter(prontuario=post_data.get("prontuario")).exists()
 
 
 class TestOfertaDisciplinaListView(TestCase):
@@ -50,15 +53,14 @@ class TestOfertaDisciplinaListView(TestCase):
         )
 
     def test_get_list_of_oferta_disciplina(self):
+        # Teste para verificar se a lista de ofertas de disciplinas é retornada corretamente
         oferta_disciplina = make(OfertaDisciplina)
         response = self.client.get(self.url)
         oferta_disciplina_list = response.context["object_list"]
         assert list(oferta_disciplina_list) == [oferta_disciplina]
 
 
-class ParticipacaoCreateViewTest(TestCase):
-    """Unit tests for ParticipacaoCreateView"""
-
+class TestParticipacaoCreateView(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(
@@ -67,7 +69,7 @@ class ParticipacaoCreateViewTest(TestCase):
         self.aluno = Aluno.objects.create(user=self.user)
 
     def test_successful_enrollment(self):
-        """Test successful enrollment for a single course"""
+        # Teste para verificar a inscrição bem-sucedida em uma disciplina
         turma = make(Turma, aluno=[])
         for index in range(1, 11):
             aluno = make(Aluno, prontuario=index)
@@ -82,35 +84,26 @@ class ParticipacaoCreateViewTest(TestCase):
             professor=professor,
             sala=sala,
         )
-
         oferta_ids = [oferta_disciplina.pk]
-
         self.client.login(email="testuser@mail.com", password="secret")
-
         response = self.client.post(
             reverse("participacao_create"), {"oferta-disciplina": oferta_ids}
         )
-
-        # Assert successful enrollment
         assert response.status_code == 302
-        assert reverse("home_page") == response.url
+        assert response.url == reverse("home_page")
         assert Participacao.objects.filter(aluno=self.aluno).count() == 1
 
     def test_exceeded_credits_error(self):
-        """Test error handling for exceeding credit limit"""
+        # Teste para verificar erro ao exceder o limite de créditos
         make(Participacao, aluno=self.aluno, _quantity=3)
         oferta_disciplina = make(OfertaDisciplina)
         oferta_ids = [oferta_disciplina.pk]
-
         self.client.login(email="testuser@mail.com", password="secret")
-
         response = self.client.post(
             reverse("participacao_create"), {"oferta-disciplina": oferta_ids}
         )
-
-        # Assert error handling for exceeding credits
         assert response.status_code == 302
-        assert reverse("oferta_disciplina_list") == response.url
+        assert response.url == reverse("oferta_disciplina_list")
         assert Participacao.objects.filter(aluno=self.aluno).count() == 3
         assert (
             str(list(get_messages(response.wsgi_request))[0])
@@ -118,6 +111,7 @@ class ParticipacaoCreateViewTest(TestCase):
         )
 
     def test_exceeded_credits_error_on_creation(self):
+        # Teste para verificar erro ao exceder o limite de créditos na criação da participação
         ofertas_disciplinas = make(OfertaDisciplina, _quantity=4)
         oferta_ids = [
             ofertas_disciplinas[0].pk,
@@ -131,17 +125,42 @@ class ParticipacaoCreateViewTest(TestCase):
         response = self.client.post(
             reverse("participacao_create"), {"oferta-disciplina": oferta_ids}
         )
-
-        # Assert error handling for exceeding credits
         assert response.status_code == 302
-        assert reverse("oferta_disciplina_list") == response.url
+        assert response.url == reverse("oferta_disciplina_list")
         assert (
             str(list(get_messages(response.wsgi_request))[0])
             == "Créditos excedidos, máximo 3"
         )
 
+    def test_dependecies_not_done(self):
+        # Teste para verificar erro ao tentar inscrever-se em uma disciplina com dependência não realizada
+        professor = make(Professor)
+        primary_disciplina = make(Disciplina, nome="Dependencia")
+        secondary_disciplina = make(
+            Disciplina, dependencia=primary_disciplina, nome="Disciplina"
+        )
+        oferta_disciplina = make(
+            OfertaDisciplina, disciplina=secondary_disciplina, professor=professor
+        )
+        oferta_ids = [oferta_disciplina.pk]
+        dependencia = Disciplina.objects.filter(
+            nome=oferta_disciplina.disciplina.dependencia.nome
+        ).first()
+
+        self.client.login(email="testuser@mail.com", password="secret")
+        response = self.client.post(
+            reverse("participacao_create"), {"oferta-disciplina": oferta_ids}
+        )
+
+        assert response.status_code == 302
+        assert response.url == reverse("oferta_disciplina_list")
+        assert (
+            str(list(get_messages(response.wsgi_request))[0])
+            == f"A disciplina {oferta_disciplina.disciplina.nome} tem como pré-requisito {dependencia.nome}, a qual você não cursou"
+        )
+
     def test_schedule_conflict_error(self):
-        """Test error handling for schedule conflict"""
+        # Teste para verificar erro de conflito de horários
         turma = make(Turma, aluno=[])
         turma.aluno.add(self.aluno)
         professor = make(Professor)
@@ -154,7 +173,6 @@ class ParticipacaoCreateViewTest(TestCase):
             professor=professor,
             sala=sala,
         )
-
         oferta_disciplina2 = make(
             OfertaDisciplina,
             turma=turma,
@@ -162,26 +180,22 @@ class ParticipacaoCreateViewTest(TestCase):
             professor=professor,
             sala=sala,
         )
-
-        oferta_ids = [oferta_disciplina1.pk, oferta_disciplina2.pk]
-
+        make(Participacao, aluno=self.aluno, ofertaDisciplina=oferta_disciplina2)
+        oferta_ids = [oferta_disciplina1.pk]
         self.client.login(email="testuser@mail.com", password="secret")
-
         response = self.client.post(
             reverse("participacao_create"), {"oferta-disciplina": oferta_ids}
         )
-
-        # Assert error handling for schedule conflict
         assert response.status_code == 302
-        assert reverse("oferta_disciplina_list") == response.url
-        assert Participacao.objects.count() == 0  # No participations created
+        assert response.url == reverse("oferta_disciplina_list")
+        assert Participacao.objects.count() == 1
         assert (
             str(list(get_messages(response.wsgi_request))[0])
             == "Há conflito de horários"
         )
 
     def test_room_capacity_error(self):
-        """Test error handling for room capacity limit"""
+        # Teste para verificar erro ao exceder a capacidade da sala
         turma = make(Turma, aluno=[])
         turma.aluno.add(self.aluno)
         professor = make(Professor)
@@ -194,27 +208,16 @@ class ParticipacaoCreateViewTest(TestCase):
             professor=professor,
             sala=sala,
         )
-
         self.client.login(email="testuser@mail.com", password="secret")
-
         oferta_ids = [oferta_disciplina.pk]
         response = self.client.post(
             reverse("participacao_create"), {"oferta-disciplina": oferta_ids}
         )
-
-        print()
-        # Assert error handling for room capacity
         assert response.status_code == 302
-        assert reverse("oferta_disciplina_list") == response.url
-        assert (
-            str(list(get_messages(response.wsgi_request))[0])
-            == f"Disciplina {oferta_disciplina.disciplina.nome} com capacidade máxima"
-        )
+        assert response.url == reverse("oferta_disciplina_list")
 
 
-class AlunoDisciplinaListViewTest(TestCase):
-    """Unit tests for AlunoDisciplinaListView"""
-
+class TestAlunoDisciplinaListView(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(
@@ -223,49 +226,185 @@ class AlunoDisciplinaListViewTest(TestCase):
         self.aluno = Aluno.objects.create(user=self.user)
 
     def test_authenticated_user_view(self):
-        """Test successful view access for an authenticated user"""
+        # Teste para verificar o acesso à visualização para um usuário autenticado
         self.client.login(email="testuser@mail.com", password="secret")
-
         response = self.client.get(reverse("aluno_disciplina_list"))
-
         assert response.status_code == 200
         self.assertTemplateUsed(response, "alunos/list.html")
 
     def test_view_with_participations(self):
-        """Test view with existing participations"""
+        # Teste para verificar a visualização com participações existentes
         oferta_disciplina1 = make(OfertaDisciplina)
         oferta_disciplina2 = make(OfertaDisciplina)
         make(Participacao, aluno=self.aluno, ofertaDisciplina=oferta_disciplina1)
         make(Participacao, aluno=self.aluno, ofertaDisciplina=oferta_disciplina2)
-
         self.client.login(email="testuser@mail.com", password="secret")
-
         response = self.client.get(reverse("aluno_disciplina_list"))
-
         assert response.status_code == 200
         self.assertTemplateUsed(response, "alunos/list.html")
-        # Assert context data contains filtered OfertaDisciplina objects
         assert response.context["object_list"] == [
             oferta_disciplina1,
             oferta_disciplina2,
         ]
 
-    def test_view_with_duplicates(self):
-        """Test view handling duplicate participations for the same course"""
-        oferta_disciplina = make(OfertaDisciplina)
-        make(
-            Participacao,
-            aluno=self.aluno,
-            ofertaDisciplina=oferta_disciplina,
-            _quantity=2,
+
+class TestOtherParticipacaoCreateView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            email="testuser@mail.com", password="secret"
+        )
+        self.client.login(email=self.user.email, password="secret")
+
+    def test_remove_disciplina_pendente(self):
+        disciplina_pendente = "Test Disciplina (some additional info)"
+        session = self.client.session
+        session["disciplinas_pendentes"] = [disciplina_pendente]
+        session.save()
+
+        post_data = {"disciplina-pendente": [disciplina_pendente]}
+        response = self.client.post(
+            reverse("other_participacao_create"), data=post_data
         )
 
+        assert response.status_code == 302
+        assert reverse("oferta_disciplina_list") == response.url
+        assert "disciplinas_pendentes" in self.client.session
+        assert len(self.client.session["disciplinas_pendentes"]) == 0
+
+
+class TestListaDeEsperaCreateView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            email="testuser@mail.com", password="secret"
+        )
+        self.aluno = Aluno.objects.create(user=self.user)
+        self.professor = make(Professor)
+        self.disciplina = make(Disciplina, nome="Test Disciplina")
+        self.oferta_disciplina = make(
+            OfertaDisciplina, disciplina=self.disciplina, professor=self.professor
+        )
+        self.client.login(email=self.user.email, password="secret")
+
+    def test_create_lista_de_espera(self):
+        # Teste da criação da lista de espera
+        disciplina_pendente = f"{self.disciplina.nome}"
+        session = self.client.session
+        session["disciplinas_pendentes"] = [disciplina_pendente]
+        session.save()
+
+        post_data = {"disciplina-pendente": [disciplina_pendente]}
+        response = self.client.post(reverse("lista_espera_create"), data=post_data)
+
+        assert response.status_code == 302
+        assert response.url == reverse("oferta_disciplina_list")
+        assert "disciplinas_pendentes" in self.client.session
+        assert len(self.client.session["disciplinas_pendentes"]) == 0
+        assert ListaDeEspera.objects.filter(
+            ofertaDisciplina=self.oferta_disciplina, aluno=self.aluno
+        ).exists()
+        assert (
+            str(list(get_messages(response.wsgi_request))[0])
+            == "Aluno inserido na lista de espera!"
+        )
+        assert "lista_de_espera" in self.client.session
+        assert str(self.oferta_disciplina) in self.client.session["lista_de_espera"]
+
+    def test_create_lista_de_espera_on_a_row(self):
+        # Teste da criação da lista de espera em sequência
+        disciplina_pendente = f"{self.disciplina.nome}"
+        session = self.client.session
+        session["lista_de_espera"] = []
+        session["disciplinas_pendentes"] = [disciplina_pendente]
+        session.save()
+
+        post_data = {"disciplina-pendente": [disciplina_pendente]}
+        response = self.client.post(reverse("lista_espera_create"), data=post_data)
+
+        assert response.status_code == 302
+        assert response.url == reverse("oferta_disciplina_list")
+        assert "disciplinas_pendentes" in self.client.session
+        assert len(self.client.session["disciplinas_pendentes"]) == 0
+        assert ListaDeEspera.objects.filter(
+            ofertaDisciplina=self.oferta_disciplina, aluno=self.aluno
+        ).exists()
+        assert (
+            str(list(get_messages(response.wsgi_request))[0])
+            == "Aluno inserido na lista de espera!"
+        )
+        assert "lista_de_espera" in self.client.session
+        assert str(self.oferta_disciplina) in self.client.session["lista_de_espera"]
+
+
+class TestParticipacaoDeleteView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            email="testuser@mail.com", password="secret"
+        )
+        self.aluno = Aluno.objects.create(user=self.user)
+        self.disciplina = make(Disciplina, nome="Test Disciplina")
+        self.oferta_disciplina = make(OfertaDisciplina, disciplina=self.disciplina)
+        self.participacao = make(
+            Participacao, ofertaDisciplina=self.oferta_disciplina, aluno=self.aluno
+        )
+
+    def test_successful_delete_participacao(self):
         self.client.login(email="testuser@mail.com", password="secret")
 
-        response = self.client.get(reverse("aluno_disciplina_list"))
+        post_data = {"disciplina-solicitada": [self.disciplina.nome]}
+        response = self.client.post(reverse("participacao_delete"), data=post_data)
 
-        assert response.status_code == 200
-        self.assertTemplateUsed(response, "alunos/list.html")
-        # Assert context data contains only one OfertaDisciplina object
-        assert len(response.context["object_list"]) == 1
-        assert response.context["object_list"][0] == oferta_disciplina
+        assert response.status_code == 302
+        assert reverse("aluno_disciplina_list") == response.url
+        assert (
+            Participacao.objects.filter(
+                aluno=self.aluno, ofertaDisciplina=self.oferta_disciplina
+            ).count()
+            == 0
+        )
+        assert (
+            str(list(get_messages(response.wsgi_request))[0])
+            == "Inscrição cancelada com sucesso!"
+        )
+
+
+class TestListaDeEsperaDeleteView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            email="testuser@mail.com", password="secret"
+        )
+        self.aluno = Aluno.objects.create(user=self.user)
+        self.disciplina = make(Disciplina, nome="Test Disciplina")
+        self.oferta_disciplina = make(OfertaDisciplina, disciplina=self.disciplina)
+        self.lista_de_espera = make(
+            ListaDeEspera, ofertaDisciplina=self.oferta_disciplina, aluno=[self.aluno]
+        )
+
+    def test_successful_delete_lista_de_espera(self):
+        self.client.login(email="testuser@mail.com", password="secret")
+
+        disciplina_solicitada = f"{self.disciplina.nome}"
+        session = self.client.session
+        session["lista_de_espera"] = [disciplina_solicitada]
+        session.save()
+
+        post_data = {"disciplina-solicitada": [disciplina_solicitada]}
+        response = self.client.post(reverse("lista_espera_delete"), data=post_data)
+
+        assert response.status_code == 302
+        assert reverse("aluno_disciplina_list") == response.url
+        assert (
+            ListaDeEspera.objects.filter(
+                aluno=self.aluno, ofertaDisciplina=self.oferta_disciplina
+            ).count()
+            == 0
+        )
+        assert (
+            str(list(get_messages(response.wsgi_request))[0])
+            == "Inscrição na lista de espera cancelada com sucesso!"
+        )
+        assert "lista_de_espera" in self.client.session
+        assert len(self.client.session["lista_de_espera"]) == 0
